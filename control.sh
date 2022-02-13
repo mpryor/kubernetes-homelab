35acd3a1a9415312457aa4d2b668c842bb224520
mpryor <mapryor1991@yahoo.com>
2022-02-13 15:50:59 -0700

    added proper flags for node ip advertising, and for flannel interface

--- control.sh
+++ control.sh
@@ -4,7 +4,7 @@ MY_IP=$(ip addr show | grep -i 192 | awk '{print $2}' | sed "s/\/24//g")
 # Write control plane IP to shared dir
 echo $MY_IP > /vagrant/control-ip
 
-curl -sfL https://get.k3s.io | sh -s - --advertise-address $MY_IP
+curl -sfL https://get.k3s.io | sh -s - --node-ip $MY_IP --flannel-iface enp0s8
 
 # Copying server token to shared directory to be used by worker nodes
 cp /var/lib/rancher/k3s/server/node-token /vagrant
--- worker.sh
+++ worker.sh
@@ -1,4 +1,6 @@
+MY_IP=$(ip addr show | grep -i 192 | awk '{print $2}' | sed "s/\/24//g") 
+
 CONTROL_IP="$(cat /vagrant/control-ip):6443"
 SERVER_TOKEN=$(cat /vagrant/node-token)
 
-curl -sfL https://get.k3s.io | K3S_URL=https://$CONTROL_IP K3S_TOKEN=$SERVER_TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://$CONTROL_IP K3S_TOKEN=$SERVER_TOKEN sh -s - --flannel-iface enp0s8 --node-ip $MY_IP
