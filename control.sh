ebf2821ad018dfb3703703ef9704ab983d2387b6
mpryor <mapryor1991@yahoo.com>
2022-02-12 17:51:40 -0700

    formatting

--- control.sh
+++ control.sh
@@ -8,4 +8,3 @@ curl -sfL https://get.k3s.io | sh -s - --advertise-address $MY_IP
 
 # Copying server token to shared directory to be used by worker nodes
 cp /var/lib/rancher/k3s/server/node-token /vagrant
-
